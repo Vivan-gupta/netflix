2685b6455824cb986b18f962734b09303c794294
Vivan-gupta <131210432+Vivan-gupta@users.noreply.github.com>
2023-12-15 00:15:26 +0530

    Add files via upload

---
+++ netflix.js
@@ -0,0 +1,5 @@
+function playMovie(videoSource) {
+  // You can implement the logic to play the selected movie here
+  alert(`Playing: ${videoSource}`);
+  // For a real implementation, you would use a video player library or HTML5 video element.
+}
